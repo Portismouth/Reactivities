7dea8ed351e4011d65d9878909218c9624039641
portismouth <ozzy.gonzalez@gmail.com>
2020-09-07 12:36:47 -0500

    end of section 4

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,23 +1,18 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Application.Activities;
+using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Persistence;
 
 namespace API
 {
-    public class Startup
+  public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup (IConfiguration configuration)
         {
             Configuration = configuration;
         }
@@ -25,39 +20,44 @@ namespace API
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
+        public void ConfigureServices (IServiceCollection services)
         {
-            services.AddDbContext<DataContext>(opt => {
-                opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
-            });
-            services.AddCors(opt => 
+            services.AddDbContext<DataContext> (opt =>
             {
-                opt.AddPolicy("CorsPolicy", policy => {
-                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+                opt.UseSqlite (Configuration.GetConnectionString ("DefaultConnection"));
+            });
+
+            services.AddCors (opt =>
+            {
+                opt.AddPolicy ("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyHeader ().AllowAnyMethod ().WithOrigins ("http://localhost:3000");
                 });
             });
-            services.AddControllers();
+
+            services.AddMediatR (typeof (List.Handler).Assembly);
+            services.AddControllers ();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure (IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
+            if (env.IsDevelopment ())
             {
-                app.UseDeveloperExceptionPage();
+                app.UseDeveloperExceptionPage ();
             }
 
             // app.UseHttpsRedirection();
 
-            app.UseRouting();
+            app.UseRouting ();
 
-            app.UseAuthorization();
+            app.UseAuthorization ();
 
-            app.UseCors("CorsPolicy");
+            app.UseCors ("CorsPolicy");
 
-            app.UseEndpoints(endpoints =>
+            app.UseEndpoints (endpoints =>
             {
-                endpoints.MapControllers();
+                endpoints.MapControllers ();
             });
         }
     }
